fb10d90ef25f8126dd694bd7dacbf5d9a6d6b876
delru <ulidebonis198@gmail.com>
2025-04-29 18:03:45 -0300

    Update MainActivity.kt

--- app/src/main/java/com/example/parcial29mar/MainActivity.kt
+++ app/src/main/java/com/example/parcial29mar/MainActivity.kt
@@ -36,10 +36,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
 @Preview(showBackground = true)
